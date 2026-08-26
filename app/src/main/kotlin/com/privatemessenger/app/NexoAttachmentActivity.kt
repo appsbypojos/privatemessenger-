f67e519e5ed1bc03ffd617ef8b5374658fd5fa8e
pojos <vda45lol@gmail.com>
2026-08-26 10:43:50 +0800

    fix: cancel attachment coroutine scope on destroy

--- app/src/main/kotlin/com/privatemessenger/app/NexoAttachmentActivity.kt
+++ app/src/main/kotlin/com/privatemessenger/app/NexoAttachmentActivity.kt
@@ -9,12 +9,13 @@ import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.OkHttpClient
 import org.json.JSONObject
 import java.util.UUID
 
